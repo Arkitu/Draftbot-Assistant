8f7ac8f18006bd78341448ac4c82e45862a6f6e2
Overgame360 <maxime@tlon.fr>
2022-05-09 20:09:54 +0200

    Rajout de la cmd /reminders

---
+++ commands/everyone/reminders.js
@@ -0,0 +1,46 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbed } from 'discord.js';
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig.js';
+
+export const data = new SlashCommandBuilder()
+	.setName('reminders')
+	.setDescription('Renvois la liste des rappels');
+export async function execute(interaction) {
+	await interaction.deferReply();
+	const db = new JsonDB(new Config("db", true, false, '/'));
+	const config = new JsonDB(new Config("config", true, false, '/'));
+
+	let reminders = db.getData('/reminders').filter(reminder => reminder.author_id == interaction.user.id);
+
+	let str_reminders = "";
+	if (reminders.length > 0) {
+		for (let reminder of reminders) {
+			let delay = reminder.dead_line_timestamp - Date.now();
+			let str_date = "";
+			if (delay > 86400000) {
+				str_date = `${Math.floor(delay / 86400000)} jour(s) `;
+			} else if (delay > 3600000) {
+				str_date = `${Math.floor(delay / 3600000)}h ${Math.floor(delay % 3600000 / 60000)}min`;
+			} else if (delay > 60000) {
+				str_date = `${Math.floor(delay / 60000)}min ${Math.floor(delay % 60000 / 1000)}sec`;
+			} else if (delay > 1000) {
+				str_date = `${Math.floor(delay / 1000)}sec`;
+			} else if (delay > 0) {
+				str_date = `${delay}ms`;
+			} else {
+				str_date = "maintenant";
+			}
+			str_reminders += `• **message :** ${reminder.message} - **temps restant :** ${str_date} - **salon :** <#${reminder.channel.channel_id}>\n`;
+		}
+	} else {
+		str_reminders = "Vous n'avez aucun rappel";
+	}
+
+	let embed = new MessageEmbed()
+		.setColor(config.getData("/main_color"))
+		.setTitle(`Rappels de ${interaction.user.username}`)
+		.setDescription(str_reminders);
+	
+	interaction.editReply({ embeds: [embed] });
+}
