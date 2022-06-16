a197c753744648551f86fa36cdc0c6abaed53f89
Overgame360 <maxime@tlon.fr>
2022-06-16 15:08:05 +0200

    encore

--- commands/admin/fetch.js
+++ commands/admin/fetch.js
@@ -11,43 +11,49 @@ export const data = new SlashCommandBuilder()
     );
 export async function execute(interaction, config, db) {
     let thing;
+    let type = "(i don't know)";
 	try {
         thing = await interaction.client.users.fetch(interaction.options.getString('id'));
-        console.log("This is an user");
+        type = "user";
     } catch {
         console.log("Not a user");
     }
     try {
         thing = await interaction.client.channels.fetch(interaction.options.getString('id'));
-        console.log("This is a channel");
+        type = "channel";
     } catch {
         console.log("Not a channel");
     }
     try {
         thing = await interaction.client.guilds.fetch(interaction.options.getString('id'));
-        console.log("This is a guild");
+        type = "guild";
     } catch {
         console.log("Not a guild");
     }
     try {
         thing = await interaction.client.emojis.fetch(interaction.options.getString('id'));
-        console.log("This is an emoji");
+        type = "emoji";
     } catch {
         console.log("Not an emoji");
     }
     try {
         thing = await interaction.client.webhooks.fetch(interaction.options.getString('id'));
-        console.log("This is a webhook");
+        type = "webhook";
     } catch {
         console.log("Not a webhook");
     }
     try {
         thing = await interaction.client.invites.fetch(interaction.options.getString('id'));
-        console.log("This is an invite");
+        type = "invite";
     } catch {
         console.log("Not an invite");
     }
+    console.log("This is a" + type)
     console.log("The thing :");
     console.log(thing);
+    if (type == "channel") {
+        console.log("Permissions :");
+        console.log(thing.permissionOverwrites.cache);
+    }
     await interaction.reply(`${thing}`);
 }
