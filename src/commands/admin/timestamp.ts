f492b9b8421033d9b4cf0cadba45717432070950
Overgame360 <maxime@tlon.fr>
2022-07-31 17:20:45 +0200

    transition de /timestamp

--- src/commands/admin/timestamp.ts
+++ src/commands/admin/timestamp.ts
@@ -1,8 +1,9 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { Context } from '../../libs/Context.js';
 
 export const data = new SlashCommandBuilder()
 	.setName('timestamp')
 	.setDescription('Affiche le timestamp actuel');
-export async function execute(interaction, config, db, constants) {
-	await interaction.reply(`${Date.now()}`);
+export async function execute(ctx: Context) {
+	await ctx.interaction.reply(`${Date.now()}`);
 }
