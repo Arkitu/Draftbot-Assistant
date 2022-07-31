52cfa012c1f5ada461c6b4ce9ae2a4abc775eb9a
Overgame360 <maxime@tlon.fr>
2022-07-31 15:06:22 +0200

    transition de /bitfield_to_permission en ts + création d'un fichier de déclaration ts pour la lib discord-bitfield-calculator

--- src/commands/admin/bitfield_to_permissions.ts
+++ src/commands/admin/bitfield_to_permissions.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import * as bitfieldCalculator from 'discord-bitfield-calculator';
+import bitfieldCalculator from 'discord-bitfield-calculator';
+import { Context } from '../../libs/Context.js';
 
 export const data = new SlashCommandBuilder()
 	.setName('bitfield_to_permissions')
@@ -10,9 +11,9 @@ export const data = new SlashCommandBuilder()
             .setRequired(true)
             .setDescription('Le bitfield à convertir')
     );
-export async function execute(interaction, config, db, constants) {
-    let permissions = bitfieldCalculator.permissions(interaction.options.getString('bitfield'));
+export async function execute(ctx: Context) {
+    let permissions = bitfieldCalculator.permissions(ctx.interaction.options.getString('bitfield'));
     console.log("Permissions :");
     console.log(permissions);
-    await interaction.reply(`${permissions}`);
+    await ctx.interaction.reply(`${permissions}`);
 }
---
+++ src/types/discord-bitfield-calculator.d.ts
@@ -0,0 +1,7 @@
+declare module "discord-bitfield-calculator" {
+    const bitfieldCalculator: {
+        permissions(bitfield: string): any[];
+    }
+
+    export default bitfieldCalculator;
+}
