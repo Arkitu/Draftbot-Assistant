521210734c37c7ef51c92ecb3021492101cad202
Overgame360 <maxime@tlon.fr>
2022-08-01 10:33:10 +0200

    transition de Goal.js

--- src/libs/Goal.ts
+++ src/libs/Goal.ts
@@ -1,10 +1,22 @@
 import { createHash } from "crypto";
 import { Context } from "./Context.js";
+import { User } from "discord.js";
 
 export class Goal {
+
+    ctx: Context;
+    user: User | undefined;
+    user_id: string;
+    start: Date;
+    end: Date;
+    value: number;
+    unit: string;
+    init_value: number;
+    end_value: number;
+
     constructor(opts: {
         ctx: Context,
-        user_id: any,
+        user_id: string,
         start: Date,
         end: Date,
         value: number,
@@ -12,21 +24,22 @@ export class Goal {
         init_value: number,
         end_value: number
     }) {
-        this.client = client;
-        this.db = db;
-        this.config = config;
-        this.user = client.users.fetch(user_id);
-        this.user_id = user_id;
-        this.start = start;
-        this.end = end;
-        this.value = value;
-        this.unit = unit || "rank_points";
-        this.init_value = init_value;
-        this.end_value = end_value;
+        this.ctx = opts.ctx;
+        this.user_id = opts.user_id;
+        this.start = opts.start;
+        this.end = opts.end;
+        this.value = opts.value;
+        this.unit = opts.unit || "rank_points";
+        this.init_value = opts.init_value;
+        this.end_value = opts.end_value;
+    }
+
+    async init () {
+        this.user = await this.ctx.client.users.fetch(this.user_id);
     }
 
     async save() {
-        this.db.push(`/users/${createHash('md5').update(this.user_id).digest('hex')}/config/goal`, {
+        this.ctx.db.push(`/users/${createHash('md5').update(this.user_id).digest('hex')}/config/goal`, {
             start: this.start.getTime(),
             end: this.end.getTime(),
             value: this.value,
