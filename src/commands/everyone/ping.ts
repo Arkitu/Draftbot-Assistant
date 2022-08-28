15ccff04bad33b357f1cf252e7d4e8d2fd8b1cf6
Overgame360 <maxime@tlon.fr>
2022-08-28 22:23:15 +0200

    mise à jour de /ping

--- src/commands/everyone/ping.ts
+++ src/commands/everyone/ping.ts
@@ -5,5 +5,5 @@ export const data = new SlashCommandBuilder()
 	.setName('ping')
 	.setDescription('Répond pong');
 export async function execute(interaction: CommandInteraction) {
-	await ctx.interaction.reply(':ping_pong: Pong !');
+	await interaction.reply(':ping_pong: Pong !');
 }
