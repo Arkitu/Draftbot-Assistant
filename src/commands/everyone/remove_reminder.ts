84feff83c3e09ecc4fee979063d43811056605db
Overgame360 <maxime@tlon.fr>
2022-08-29 12:06:53 +0200

    Mise à jour de /remove_reminder

--- src/commands/everyone/remove_reminder.ts
+++ src/commands/everyone/remove_reminder.ts
@@ -1,41 +1,32 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
-import Reminder from '../../libs/Reminder.js';
-import { DB_Reminder } from '../../libs/Interfaces.js';
-import { TextBasedChannel, User } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
 	.setName('remove_reminder')
 	.setDescription('Supprime un rappel')
-    .addStringOption(option => option
-        .setName("message")
-        .setDescription("Le message du rappel")
-        .setRequired(true)
-    );
+    .addIntegerOption(opt=>
+        opt
+            .setName("id")
+            .setDescription("L'id du rappel (faites /reminders pour le connaître)")
+            .setRequired(true)
+    )
 export async function execute(interaction: CommandInteraction) {
     await interaction.deferReply();
     let opt = {
-        msg: interaction.options.getString("message")
+        id: interaction.options.getInteger("id")
     }
-    if (db.getData(`/reminders`).filter((r: DB_Reminder) => r.author_id == interaction.user.id && r.message == opt.msg).length == 0) {
-        await interaction.editReply(":warning: Aucun rappel ne correspond à ce message");
+
+    const reminder = await models.Reminder.findByPk(opt.id);
+    if (!reminder) {
+        interaction.editReply(":warning: Ce rappel n'existe pas ! Faites `/reminders` pour connaitres vos rappels");
         return;
     }
-    for (let reminder of (db.getData(`/reminders`) as DB_Reminder[]).filter((r: DB_Reminder) => r.author_id == interaction.user.id && r.message == opt.msg)) {
-        let channel: TextBasedChannel | User;
-        let fetched = await client.channels.fetch(reminder.channel.id);
-		if (fetched.isText()) {
-			channel = fetched
-		} else {
-			channel = await client.users.fetch(reminder.channel.id)
-		}
-        new Reminder({
-            ctx: ctx,
-            channel: channel,
-            dead_line_timestamp: reminder.dead_line_timestamp,
-            message: reminder.message,
-            author: await client.users.fetch(reminder.author_id)
-        }).delete();
+    if (reminder.userId != interaction.user.id) {
+        interaction.editReply(":warning: Vous ne pouvez pas supprimer les rappels des autres ! Faites `/reminders` pour connaitres vos rappels");
+        return;
     }
-    await interaction.editReply("Rappel(s) supprimé(s) !");
+
+    reminder.destroy()
+
+    interaction.editReply("Rappel supprimé !");
 }
--- src/models/Reminder.ts
+++ src/models/Reminder.ts
@@ -1,6 +1,6 @@
 import { MessageEmbed, User as DiscordUser, TextBasedChannel, DMChannel } from 'discord.js';
 import { User } from '.';
-import { Table, Column, Model, DataType, BelongsTo } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, BelongsTo, ForeignKey } from 'sequelize-typescript';
 
 @Table
 export default class Reminder extends Model {
@@ -30,8 +30,11 @@ export default class Reminder extends Model {
     })
     message: string;
 
-    @BelongsTo(() => User)
-    author: User;
+    @BelongsTo(()=>User)
+    user: User;
+
+    @ForeignKey(()=>User)
+    userId: string;
     
     // Not saved in database
     @Column(DataType.VIRTUAL)
@@ -79,85 +82,14 @@ export default class Reminder extends Model {
 
     async sendReminderMessage(opts: {embed: MessageEmbed}) {
         await this.getChannel();
-        await this.author.fetchDiscordUser();
+        await this.user.fetchDiscordUser();
         if (this.channel instanceof DMChannel) {
             this.channel.send({embeds: [opts.embed]});
             return;
         }
         if (!("permissionsFor" in this.channel)) return;
         if (this.channel.permissionsFor(client.user).has(["SEND_MESSAGES", "EMBED_LINKS"])) {
-            this.channel.send({content: this.author.discordUser.toString(), embeds: [opts.embed]});
+            this.channel.send({content: this.user.discordUser.toString(), embeds: [opts.embed]});
         }
     }
 }
-
-/*
-export class Reminder extends Model {
-    declare id: number;
-    declare channel_id: string;
-    declare channel_isUser: boolean;
-    declare dead_line: number;
-    declare message: string;
-
-    ctx: Context;
-    channel: TextBasedChannel | User;
-    author: User;
-
-    constructor(opts: {
-        ctx: Context,
-        channel: TextBasedChannel | User,
-        dead_line: number,
-        message: string,
-        author: User
-    }) {
-        super({
-            channel_id: opts.channel.id,
-            channel_isUser: opts.channel instanceof User,
-            dead_line: opts.dead_line,
-            message: opts.message,
-            author_id: opts.author.id
-        })
-        this.ctx = opts.ctx;
-        this.channel = opts.channel;
-        this.author = opts.author;
-    }
-
-    async start() {
-        setTimeout(async () => {
-            if (await this.ctx.sequelize.models.reminder.findOne({ where: { id: this.id } })) {
-                let embed = new MessageEmbed()
-                    .setColor(this.ctx.config.getData("/main_color"))
-                    .setTitle("Reminder")
-                    .setDescription(this.message)
-                await this.sendReminderMessage(embed);
-                this.destroy()
-            }
-        }, (this.dead_line - Date.now()));
-        return this;
-    }
-
-    async sendReminderMessage(embed: MessageEmbed) {
-        if (this.channel instanceof DMChannel) {
-            await this.channel.send({embeds: [embed]});
-            return;
-        }
-        if (!("permissionsFor" in this.channel)) return;
-        if (this.channel.permissionsFor(this.ctx.client.user).has(["SEND_MESSAGES", "EMBED_LINKS"])) {
-            await this.channel.send({content: this.author.toString(), embeds: [embed]});
-        }
-    }
-}
-
-export const reminder_init_args: ModelAttributes = {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    channel_id: DataTypes.TEXT,
-    channel_isUser: DataTypes.BOOLEAN,
-    dead_line: DataTypes.INTEGER,
-    message: DataTypes.TEXT,
-    author_id: DataTypes.TEXT
-}
-*/
