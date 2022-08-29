a273d63a2366702803471d8715643c7a8edc93a9
Overgame360 <maxime@tlon.fr>
2022-08-29 11:29:11 +0200

    Mise à jour de /reminders

--- src/commands/everyone/reminders.ts
+++ src/commands/everyone/reminders.ts
@@ -1,20 +1,23 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { MessageEmbed } from 'discord.js';
 import { CommandInteraction } from 'discord.js';
-import { DB_Reminder } from '../../libs/Interfaces.js';
 
 export const data = new SlashCommandBuilder()
 	.setName('reminders')
 	.setDescription('Renvois la liste des rappels');
 export async function execute(interaction: CommandInteraction) {
-	await ctx.interaction.deferReply();
+	await interaction.deferReply();
 
-	let reminders = ctx.db.getData('/reminders').filter((reminder: DB_Reminder) => reminder.author_id == ctx.interaction.user.id) as DB_Reminder[];
+	let reminders = await models.Reminder.findAll({
+		where: {
+			userId: interaction.user.id
+		}
+	});
 
 	let str_reminders = "";
 	if (reminders.length > 0) {
 		for (let reminder of reminders) {
-			let delay = reminder.dead_line_timestamp - Date.now();
+			let delay = reminder.deadLineTimestamp - Date.now();
 			let str_date = "";
 			if (delay > 86400000) {
 				str_date = `${Math.floor(delay / 86400000)} jour(s) `;
@@ -29,16 +32,16 @@ export async function execute(interaction: CommandInteraction) {
 			} else {
 				str_date = "maintenant";
 			}
-			str_reminders += `• **message :** ${reminder.message} - **temps restant :** ${str_date} - **salon :** <${["#", "@"][+reminder.channel.isUser]}${reminder.channel.id}>\n\n`;
+			str_reminders += `• **message :** ${reminder.message} - **temps restant :** ${str_date} - **salon :** <${["#", "@"][+reminder.channelIsUser]}${reminder.channelId}>\n\n`;
 		}
 	} else {
 		str_reminders = "Vous n'avez aucun rappel";
 	}
 
 	let embed = new MessageEmbed()
-		.setColor(ctx.config.getData("/main_color"))
-		.setTitle(`Rappels de ${ctx.interaction.user.username}`)
+		.setColor(config.getData("/main_color"))
+		.setTitle(`Rappels de ${interaction.user.username}`)
 		.setDescription(str_reminders);
 	
-	ctx.interaction.editReply({ embeds: [embed] });
+	interaction.editReply({ embeds: [embed] });
 }
