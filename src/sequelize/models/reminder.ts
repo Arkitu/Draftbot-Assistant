cec186f7b9cbf1953253be9109de6032ec63e315
Overgame360 <maxime@tlon.fr>
2022-09-09 20:30:36 +0200

    save

--- src/sequelize/models/reminder.ts
+++ src/sequelize/models/reminder.ts
@@ -1,61 +1,112 @@
-import { DataTypes, Model, ModelAttributes, Optional } from "sequelize";
-import { ModelWithAssociate SequelizeWithAssociate, snowflakeValidate } from ".";
+import { DMChannel, MessageEmbed, TextBasedChannel } from "discord.js";
+import { DataTypes, Model, ModelAttributes, Optional, Sequelize } from "sequelize";
+import { ModelWithAssociate, SequelizeWithAssociate, snowflakeValidate } from ".";
 
-export default (db: SequelizeWithAssociate) => {
-  class Reminder extends Model {
-    declare channelId: string;
-    declare channelIsUser: boolean;
-    declare deadLineTimestamp: number;
-    declare static deadLineTimestamp: number;
-    declare static setDataValue: any;
+export class Reminder extends Model {
+  declare channelId: string;
+  declare channelIsUser: boolean;
+  declare deadLineTimestamp: number;
+  declare message: string;
+  declare deadLine: Date;
+  declare channel: TextBasedChannel;
+  declare getUser: (): User
+  declare static deadLineTimestamp: number;
+  declare static setDataValue: any;
+  declare static channelId: string;
 
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(db: SequelizeWithAssociate) {
-      this.belongsTo(db.models.User)
-    }
+  /**
+   * You need to fetch the channel before using reminder.channel
+   */
+  fetchChannel() {
+    return client.channels.fetch(this.channelId);
+  }
 
-    static get initArgs() {
-      let args: ModelAttributes<Reminder, Optional<any, never>> = {
-        channelId: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: snowflakeValidate
-        },
-        channelIsUser: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-          defaultValue: false
-        },
-        deadLineTimestamp: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-        },
-        message: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        deadLine: {
-          type: DataTypes.VIRTUAL,
-          get: ()=>{
-            return new Date(this.deadLineTimestamp);
-          },
-          set: (val: Date)=>{
-            this.setDataValue(val.getTime());
-          }
+  // Methods
+  async start() {
+    console.debug(1);
+    setTimeout(async () => {
+        console.debug(2)
+        if (await db.models.Reminder.findOne({ where: { id: this.id } })) {
+            console.debug(3)
+            console.debug(this.message, typeof this.message)
+            let embed = new MessageEmbed()
+                .setColor(config.getData("/main_color"))
+                .setTitle("Reminder")
+                .setDescription(this.get('message'))
+            this.sendReminderMessage({ embed: embed });
+            this.destroy()
         }
-      };
-      return args;
+    }, (this.deadLine.getTime() - Date.now()));
+    return this;
+  }
+
+  async sendReminderMessage(opts: {embed: MessageEmbed}) {
+    await this.fetchChannel();
+    await this.user.fetchDiscordUser();
+    if (this.channel instanceof DMChannel) {
+        this.channel.send({embeds: [opts.embed]});
+        return;
+    }
+    if (!("permissionsFor" in this.channel)) return;
+    if (this.channel.permissionsFor(client.user).has(["SEND_MESSAGES", "EMBED_LINKS"])) {
+        this.channel.send({content: this.user.discordUser.toString(), embeds: [opts.embed]});
     }
   }
 
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate() {
+    this.belongsTo(db.models.User)
+  }
+
+  static get initArgs() {
+    let args: ModelAttributes<Reminder, Optional<any, never>> = {
+      channelId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: snowflakeValidate
+      },
+      channelIsUser: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      deadLineTimestamp: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      message: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      deadLine: {
+        type: DataTypes.VIRTUAL,
+        get: ()=>{
+          return new Date(this.deadLineTimestamp);
+        },
+        set: (val: Date)=>{
+          this.setDataValue(val.getTime());
+        }
+      },
+      channel: {
+        type: DataTypes.VIRTUAL,
+        get: ()=>{
+          return client.channels.cache.get(this.channelId);
+        }
+      }
+    };
+    return args;
+  }
+}
+
+export default () => {
   Reminder.init(Reminder.initArgs, {
-    sequelize,
+    sequelize: db,
     modelName: 'Reminder',
   });
 
-  return Reminder;
+  return Reminder as ModelWithAssociate;
 };
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,15 +1,14 @@
 import Context from '../libs/Context';
 import { Client } from '../bot.js';
 import { JsonDB } from 'node-json-db';
-import { Sequelize } from 'sequelize-typescript';
 import { sequelizeModels } from '../models';
+import { SequelizeWithAssociate } from '../sequelize/models';
 
 declare global {
     var client: Client;
     var config: JsonDB;
     var constants: JsonDB;
-    var sequelize: Sequelize;
-    var models: typeof sequelizeModels;
+    var db: SequelizeWithAssociate;
 }
 
 export {};
