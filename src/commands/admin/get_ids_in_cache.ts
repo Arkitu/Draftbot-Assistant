0f9715f6e6f387b6fbc161c602866843080d17ab
Overgame360 <maxime@tlon.fr>
2022-10-02 17:02:46 +0200

    récupération d'ids

---
+++ src/commands/admin/get_ids_in_cache.ts
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Context } from '../../libs/Context.js';
+
+export const data = new SlashCommandBuilder()
+	.setName('get_ids_in_cache')
+	.setDescription('Renvoie les ids des utilisateurs en cache');
+export async function execute(ctx: Context) {
+    await ctx.interaction.deferReply();
+	
+    let ids = ctx.client.users.cache.map(user => user.id);
+
+    let str_ids = "\"" + ids.join('",\n"') + "\""
+
+    console.log(str_ids);
+
+    if (str_ids.length < 2000) {
+        await ctx.interaction.editReply(str_ids);
+    } else {
+        await ctx.interaction.editReply("Trop de résultats pour être affichés");
+    }
+}
