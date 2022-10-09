8bbed337f092c47d4869889c8ec25ae02fd81d6b
Overgame360 <maxime@tlon.fr>
2022-10-09 09:11:44 +0200

    fix

--- src/sequelize/migrations/one.ts
+++ src/sequelize/migrations/one.ts
@@ -2,7 +2,7 @@ import "../models/index.js";
 import "../../bot.js";
 
 export async function up() {
-    db.sync({ alter: true });
+    await db.sync({ alter: true });
     for (const guild of await db.models.Guild.findAll()) {
         const data = await guild.fetchData();
         guild.data = data;
