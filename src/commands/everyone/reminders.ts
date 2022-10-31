88d97c8ecb554373f0c62e60ce8f6f956f766e9c
Overgame360 <maxime@tlon.fr>
2022-10-31 18:44:50 +0100

    fix du bug visuel de /reminders

--- src/commands/everyone/reminders.ts
+++ src/commands/everyone/reminders.ts
@@ -1,104 +1,125 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { MessageEmbed, Interaction } from 'discord.js';
-import { CommandInteraction, MessageActionRow, MessageButton, User as DiscordUser } from 'discord.js';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import {
+  CommandInteraction,
+  MessageActionRow,
+  MessageButton,
+  User as DiscordUser,
+  MessageEmbed,
+  Interaction,
+} from "discord.js";
 
-async function createReminders(page: number, user: DiscordUser): Promise<{embed: MessageEmbed, components: MessageActionRow[]}> {
-	const embed = new MessageEmbed()
-		.setColor(config.getData("/main_color"))
-		.setTitle(`Rappels de ${user.username}`)
-	
-	const buttons = new MessageActionRow();
+async function createReminders(
+  page: number,
+  user: DiscordUser
+): Promise<{ embed: MessageEmbed; components: MessageActionRow[] }> {
+  const embed = new MessageEmbed()
+    .setColor(config.getData("/main_color"))
+    .setTitle(`Rappels de ${user.username}`);
 
-	const reminders = await db.models.Reminder.findAll({
-		offset: page*10,
-		limit: 10,
-		order: db.col('deadLineTimestamp'),
-		where: {
-			UserId: user.id
-		}
-	});
+  const buttons = new MessageActionRow();
 
-	if (reminders.length === 0) {
-		embed.setDescription("Vous n'avez aucun rappel")
-		return {
-			embed: embed,
-			components: []
-		};
-	}
+  const reminders = await db.models.Reminder.findAll({
+    offset: page * 10,
+    limit: 10,
+    order: db.col("deadLineTimestamp"),
+    where: {
+      UserId: user.id,
+    },
+  });
 
-	for (let reminder of reminders) {
-		embed.addFields([{
-			name: reminder.message,
-			value: `id: ${reminder.id} | déclanchement <t:${reminder.deadLineTimestamp}:R> | salon: <${["#", "@"][+reminder.channelIsUser]}${reminder.channelId}>`
-		}])
-	}
+  if (reminders.length === 0) {
+    embed.setDescription("Vous n'avez aucun rappel");
+    return {
+      embed: embed,
+      components: [],
+    };
+  }
 
-	if (
-		reminders.length > 10
-		&&
-		(page+1)*10 < await db.models.Reminder.count({
-			where: { UserId: user.id }
-		})
-	) {
-		buttons.addComponents(
-			new MessageButton()
-				.setCustomId('next_page')
-				.setStyle('SECONDARY')
-				.setEmoji('➡')
-		);
-	}
-	if (page > 0) {
-		buttons.addComponents(
-			new MessageButton()
-				.setCustomId('previous_page')
-				.setStyle('SECONDARY')
-				.setEmoji('⬅')
-		);
-	}
+  for (let reminder of reminders) {
+    embed.addFields([
+      {
+        name: reminder.message,
+        value: `id: ${reminder.id} | déclanchement <t:${Math.round(
+          reminder.deadLineTimestamp / 1000
+        )}:R> | salon: <${["#", "@"][+reminder.channelIsUser]}${
+          reminder.channelId
+        }>`,
+      },
+    ]);
+  }
 
-	const components: MessageActionRow[] = [];
+  if (
+    reminders.length > 10 &&
+    (page + 1) * 10 <
+      (await db.models.Reminder.count({
+        where: { UserId: user.id },
+      }))
+  ) {
+    buttons.addComponents(
+      new MessageButton()
+        .setCustomId("next_page")
+        .setStyle("SECONDARY")
+        .setEmoji("➡")
+    );
+  }
+  if (page > 0) {
+    buttons.addComponents(
+      new MessageButton()
+        .setCustomId("previous_page")
+        .setStyle("SECONDARY")
+        .setEmoji("⬅")
+    );
+  }
 
-	if (buttons.components.length) components.push(buttons);
+  const components: MessageActionRow[] = [];
 
-	return {
-		embed: embed,
-		components: components
-	};
+  if (buttons.components.length) components.push(buttons);
+
+  return {
+    embed: embed,
+    components: components,
+  };
 }
 
 export const data = new SlashCommandBuilder()
-	.setName('reminders')
-	.setDescription('Renvois la liste des rappels');
+  .setName("reminders")
+  .setDescription("Renvois la liste des rappels");
 export async function execute(interaction: CommandInteraction) {
-	await interaction.deferReply();
+  await interaction.deferReply();
 
-	let page = 0;
-	let reminders = await createReminders(page, interaction.user);
+  let page = 0;
+  let reminders = await createReminders(page, interaction.user);
 
-	await interaction.editReply({ embeds: [reminders.embed], components: reminders.components })
+  await interaction.editReply({
+    embeds: [reminders.embed],
+    components: reminders.components,
+  });
 
-	if (reminders.components) {
-		let msg = await interaction.fetchReply();
-		let button_listener = async (button_interaction: Interaction) => {
-			if (!button_interaction.isButton()) return;
-			if (button_interaction.message.id != msg.id) return;
-			switch (button_interaction.customId) {
-				case 'next_page':
-					page++;
-					break;
+  if (reminders.components) {
+    let msg = await interaction.fetchReply();
+    let button_listener = async (button_interaction: Interaction) => {
+      if (!button_interaction.isButton()) return;
+      if (button_interaction.message.id != msg.id) return;
+      switch (button_interaction.customId) {
+        case "next_page":
+          page++;
+          break;
 
-				case 'previous_page':
-					page--;
-					break;
-			}
-			reminders = await createReminders(page, interaction.user);
-			await button_interaction.update({ embeds: [reminders.embed], components: reminders.components });
-		};
-		interaction.client.on('interactionCreate', button_listener);
-		setTimeout(() => {
-			if (!("edit" in msg)) return;
-			msg.edit({ components: []});
-			interaction.client.removeListener('interactionCreate', button_listener);
-		}, 300000);
-	}
+        case "previous_page":
+          page--;
+          break;
+      }
+      reminders = await createReminders(page, interaction.user);
+      await button_interaction.update({
+        embeds: [reminders.embed],
+        components: reminders.components,
+      });
+    };
+    interaction.client.on("interactionCreate", button_listener);
+    setTimeout(() => {
+      if (!("edit" in msg)) return;
+      msg.edit({ components: [] });
+      interaction.client.removeListener("interactionCreate", button_listener);
+    }, 300000);
+  }
 }
