cce7870c3fb882c64b7fa00659d5c90817a7fceb
Overgame360 <maxime@tlon.fr>
2022-11-03 15:19:31 +0100

    fix du /gtop (normalement)

--- src/commands/everyone/gtop.ts
+++ src/commands/everyone/gtop.ts
@@ -38,7 +38,7 @@ async function createGtop(page: number): Promise<{embed: MessageEmbed, component
 					break;
 			}
 		}
-		description += `${emoji}${i + 1} **${guilds[i].name}** | \`Niveau ${Math.round(guilds[i].data.level*100)/100}\`\n`;
+		description += `${emoji}${i + 1} **${guilds[i].name}** | \`Niveau ${Math.round((await guilds[i].fetchData()).level*100)/100}\`\n`;
 	}
 	embed.setDescription(description);
 
