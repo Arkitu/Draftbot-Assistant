383d12c40f2dfb393de1a9e78b5d7d8e22182fe2
Overgame360 <maxime@tlon.fr>
2023-01-19 08:59:46 +0100

    send

--- src/commands/admin/send_msg.ts
+++ src/commands/admin/send_msg.ts
@@ -27,14 +27,14 @@ export async function execute(interaction: CommandInteraction) {
   await interaction.deferReply({ ephemeral: true });
   const opts = {
     content: interaction.options.getString('content', true),
-    channel: interaction.options.getChannel('channel', false) || interaction.channel,
+    channel: (interaction.options.getChannel('channel', false) || interaction.channel) as TextBasedChannel,
     channel_id: interaction.options.getString('channel_id', false)
   };
   if (opts.channel_id) {
-    opts.channel = interaction.guild.channels.cache.get(opts.channel_id);
+    opts.channel = await client.channels.fetch(opts.channel_id) as TextBasedChannel;
   }
   try {
-    await (opts.channel as TextBasedChannel).send(opts.content);
+    await opts.channel.send(opts.content);
     interaction.editReply('Message envoyé !');
   } catch (e) {
     interaction.editReply('Une erreur est survenue :\n```' + e + '\n```');
